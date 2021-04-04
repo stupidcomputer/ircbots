4ef788491fe9839e3717d5705171f36455a65353
randomuser <randomuser@tilde.club>
2021-04-04 02:15:56 +0000

    maybe not dox myself

--- bot.py
+++ bot.py
@@ -14,6 +14,25 @@ import text
 
 kickees = Nicklist()
 exempt = Nicklist()
+MESSAGES = {
+    "revenge": "no u",
+    "kick": "error 418",
+    "quit": "bonk",
+    "kickadd": "user added to kicklist",
+    "kickadderr": "adding failed: user on exemption list",
+    "kicklist": "kickees: ",
+    "kickrm": "user removed from kicklist",
+    "kickrmerr": "error removing user from kicklist",
+    "exemptadd": "user added to exemptlist",
+    "exemptlist": "exempt: ",
+    "exemptrm": "user removed from exemptlist",
+    "exemptrmerr": "error removing user from exemptlist",
+    "writeexempt": "writing exempt data",
+    "writekickee": "writing kickee data",
+    "writekickeehostmask": "writing kickee_hostmask data",
+    "writedone": "done!",
+    "writeerr": "error writing files"
+}
 CHANNEL = "#chaos"
 PASSWORD = "hellothereperson"
 BOTNAME = "rndbotmerp"
