e1e937a4af154d117fac8232160312aa30105e1f
randomuser <randomuser@tilde.club>
2021-07-21 16:41:16 -0500

    whoops! forgot to add lang.py

---
+++ lang.py
@@ -0,0 +1,7 @@
+lang = {
+    "noduck": "there was no duck! you missed by {} seconds!",
+    "noduckstart": "there was no duck!",
+    "duckcought": "duck has been cought by {} in channel {} in {} seconds!",
+    "duck": "・゜゜・。。・゜゜\_o< QUACK!",
+    "stats": "{} has befriended {} ducks in {} different channels, having a befriend/loss ratio of {}.",
+}
