35eeaa11544749d9c8b3ac6a9393f5fded7c8610
randomuser <randomuser@tilde.club>
2021-08-02 21:13:12 -0500

    modbot is licensed under the AGPL 3.0

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+# modbot is copyright rndusr/randomuser 2021
+# This file is part of modbot.
+
+# modbot is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public
+# License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any
+# later version.
+
+# modbot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero
+# General Public License along with modbot.  If not, see
+# <https://www.gnu.org/licenses/>.
+
 import asyncio
 from importlib import import_module as mod_include
 
