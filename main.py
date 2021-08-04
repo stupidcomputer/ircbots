2f7d6322870c9741e9003de77e622335ab5559a0
randomuser <randomuser@tilde.club>
2021-08-03 22:32:37 -0500

    import template

---
+++ main.py
@@ -0,0 +1,34 @@
+import asyncio
+
+from irctokens import build, Line
+from ircrobots import Bot as BaseBot
+from ircrobots import Server as BaseServer
+from ircrobots import ConnectionParams
+
+SERVERS = [
+    ("freenode", "chat.freenode.invalid")
+]
+
+class Server(BaseServer):
+    async def line_read(self, line: Line):
+        print(f"{self.name} < {line.format()}")
+        if line.command == "001":
+            print(f"connected to {self.isupport.network}")
+            await self.send(build("JOIN", ["#testchannel"]))
+    async def line_send(self, line: Line):
+        print(f"{self.name} > {line.format()}")
+
+class Bot(BaseBot):
+    def create_server(self, name: str):
+        return Server(self, name)
+
+async def main():
+    bot = Bot()
+    for name, host in SERVERS:
+        params = ConnectionParams("BitBotNewTest", host, 6697, True)
+        await bot.add_server(name, params)
+
+    await bot.run()
+
+if __name__ == "__main__":
+    asyncio.run(main())
